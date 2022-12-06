53607b9a57144a33b0e268fe49dea70aa83959cf
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-12-06 13:20:32 +0100

    add migration "apartments" + model

---
+++ database/migrations/2022_12_06_100838_create_apartments_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            $table->string('apartment_slug')->unique()->nullable(false);
+            $table->string('apartment_title', 255)->nullable(false);
+            $table->text('description')->nullable(false);
+            $table->tinyInteger('bedrooms')->nullable(false)->default(0);
+            $table->tinyInteger('bed')->nullable()->default();
+            $table->tinyInteger('bathrooms')->nullable()->default();
+            $table->decimal('sqm',5,2)->nullable()->default(0);
+            $table->string('category', 255)->nullable()->default();
+            $table->decimal('price', 8,2)->nullable()->default(0);
+            $table->boolean('confirmed')->nullable()->default(false);
+            $table->string('apartment_images',255)->nullable();
+            $table->string('address',255)->nullable();
+            $table->tinyInteger('civic_number')->nullable();
+            $table->string('state',255)->nullable();
+            $table->decimal('latitude',12,10)->nullable(false)->default(0);
+            $table->decimal('longitude',12,10)->nullable(false)->default(0);
+            $table->string('city',255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartments');
+    }
+}
