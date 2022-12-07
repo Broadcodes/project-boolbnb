839e86694be5f4ce111edae6cf77dcf248296b16
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-12-07 12:52:38 +0100

    Create migrate table apartments and services (manyTomany)

--- database/migrations/2022_12_07_114459_create_apartment_service_table.php
+++ database/migrations/2022_12_07_114459_create_apartment_service_table.php
@@ -14,8 +14,13 @@ class CreateApartmentServiceTable extends Migration
     public function up()
     {
         Schema::create('apartment_service', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->unsignedBigInteger('apartment_id');
+            $table->foreign('apartment_id')->references('id')->on('apartments');
+
+            $table->unsignedBigInteger('service_id');
+            $table->foreign('service_id')->references('id')->on('services');
+
+            $table->primary(['apartment_id', 'service_id']);
         });
     }
 
