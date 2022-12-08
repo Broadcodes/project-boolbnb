11a6d7a35e1c1ac458dc60f88b460ccb76d3dcee
nicolacaddeo <nicola.caddeo58@gmail.com>
2022-12-08 18:00:45 +0100

    completed account information

--- resources/views/ura/dashboard.blade.php
+++ resources/views/ura/dashboard.blade.php
@@ -12,8 +12,19 @@
             <div class="row">
                 <div class="col">
                     <div class="nc-accountInfo d-flex">
-                        <div class="nc-card">
-
+                        <div class="nc-card d-flex flex-column align-items-center">
+                            <img class="nc-profilePic mt-3" src="{{ url('/images/profile-pic.png') }}" alt="profile pic">
+                            <h3>Nicola Caddeo</h3>
+                            <div class="personalInfo">
+                                <p>
+                                    <strong>Email:</strong>
+                                    <span>nicola.caddeo58@gmail.com</span>
+                                </p>
+                                <p>
+                                    <strong>Data di nascita:</strong>
+                                    <span>11/09/1999</span>
+                                </p>
+                            </div>
                         </div>
                         <div class="nc-userAction">
 
