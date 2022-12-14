785baf760a6011813c5871e1472bc826c0d545bb
nicolacaddeo <nicola.caddeo58@gmail.com>
2022-12-14 23:06:48 +0100

    client-side validation for create form

--- resources/views/ura/apartments/create.blade.php
+++ resources/views/ura/apartments/create.blade.php
@@ -17,7 +17,8 @@
 
             <div class="p-3">
                 <label class="px-2" for="apartment_title">{{ __('Titolo annuncio') }}</label>
-                <input type="text" name="apartment_title" id="apartment_title" value="{{ old('apartment_title', '') }}">
+                <input type="text" name="apartment_title" id="apartment_title" value="{{ old('apartment_title', '') }}"
+                    required maxlength="255">
                 @error('apartment_title')
                     <h5 class="text-danger ml-4">{{ $message }}</h5>
                 @enderror
@@ -25,7 +26,7 @@
 
             <div class="p-3">
                 <label class="px-2" for="description">{{ __('Descrizione') }}</label>
-                <textarea name="description" id="description" cols="30" rows="10">{{ old('description', '') }}</textarea>
+                <textarea name="description" id="description" cols="30" rows="10" required>{{ old('description', '') }}</textarea>
                 @error('description')
                     <h5 class="text-danger ml-4">{{ $message }}</h5>
                 @enderror
@@ -35,7 +36,8 @@
                 <label class="px-2" for="bedrooms">{{ __('Numero di stanze') }}</label>
                 <select name="bedrooms" id="bedrooms">
                     @for ($i = 1; $i <= 10; $i++)
-                        <option value="{{ $i }}" {{ old('bedrooms') == $i ? "selected" : ""}}>{{ $i }}</option>
+                        <option value="{{ $i }}" {{ old('bedrooms') == $i ? 'selected' : '' }}>
+                            {{ $i }}</option>
                     @endfor
                 </select>
                 @error('bedrooms')
@@ -47,7 +49,8 @@
                 <label class="px-2" for="bed">{{ __('Numero di letti') }}</label>
                 <select name="bed" id="bed">
                     @for ($i = 1; $i <= 10; $i++)
-                        <option value="{{ $i }}" {{ old('bed') == $i ? "selected" : ""}}>{{ $i }}</option>
+                        <option value="{{ $i }}" {{ old('bed') == $i ? 'selected' : '' }}>{{ $i }}
+                        </option>
                     @endfor
                 </select>
                 @error('bed')
@@ -59,7 +62,8 @@
                 <label class="px-2" for="bathrooms">{{ __('Numero di bagni') }}</label>
                 <select name="bathrooms" id="bathrooms">
                     @for ($i = 1; $i <= 10; $i++)
-                        <option value="{{ $i }}" {{ old('bathrooms') == $i ? "selected" : ""}}>{{ $i }}</option>
+                        <option value="{{ $i }}" {{ old('bathrooms') == $i ? 'selected' : '' }}>
+                            {{ $i }}</option>
                     @endfor
                 </select>
                 @error('bathrooms')
@@ -69,7 +73,8 @@
 
             <div class="p-3">
                 <label class="px-2" for="sqm">{{ __('MQ') }}</label>
-                <input type="number" name="sqm" id="sqm" step=".01" value="{{ old('sqm', '') }}">
+                <input type="number" name="sqm" id="sqm" step=".01" value="{{ old('sqm', '') }}"
+                    max="999.99" required>
                 @error('sqm')
                     <h5 class="text-danger ml-4">{{ $message }}</h5>
                 @enderror
@@ -79,7 +84,8 @@
                 <label class="px-2" for="category">{{ __('Categoria') }}</label>
                 <select name="category" id="category">
                     @foreach ($categories as $category)
-                        <option value="{{ $category }}" {{ old('category') == $category ? "selected" : ""}}>{{ $category }}</option>
+                        <option value="{{ $category }}" {{ old('category') == $category ? 'selected' : '' }}>
+                            {{ $category }}</option>
                     @endforeach
                 </select>
                 @error('category')
@@ -89,7 +95,8 @@
 
             <div class="p-3">
                 <label class="px-2" for="price">{{ __('Prezzo') }}</label>
-                <input type="number" name="price" id="price" step=".01" value="{{ old('price', '') }}">
+                <input type="number" name="price" id="price" step=".01" value="{{ old('price', '') }}" required
+                    max="999999.99">
                 @error('price')
                     <h5 class="text-danger ml-4">{{ $message }}</h5>
                 @enderror
@@ -106,7 +113,8 @@
 
             <div class="p-3">
                 <label class="px-2" for="address">{{ __('Indirizzo') }}</label>
-                <input type="text" name="address" id="address" value="{{ old('address', '') }}">
+                <input type="text" name="address" id="address" value="{{ old('address', '') }}" required
+                    maxlength="255">
                 @error('address')
                     <h5 class="text-danger ml-4">{{ $message }}</h5>
                 @enderror
@@ -114,7 +122,7 @@
 
             <div class="p-3">
                 <label class="px-2" for="civic_number">{{ __('Civico') }}</label>
-                <input type="number" name="civic_number" id="civic_number" value="{{ old('civic_number', '') }}">
+                <input type="number" name="civic_number" id="civic_number" value="{{ old('civic_number', '') }}" required>
                 @error('civic_number')
                     <h5 class="text-danger ml-4">{{ $message }}</h5>
                 @enderror
@@ -122,7 +130,8 @@
 
             <div class="p-3">
                 <label class="px-2" for="postalCode">{{ __('CAP') }}</label>
-                <input type="text" name="postalCode" id="postalCode" maxlength='5' value="{{ old('postalCode', '') }}">
+                <input type="text" name="postalCode" id="postalCode" maxlength='5' required
+                    value="{{ old('postalCode', '') }}">
                 @error('postalCode')
                     <h5 class="text-danger ml-4">{{ $message }}</h5>
                 @enderror
@@ -130,7 +139,8 @@
 
             <div class="p-3">
                 <label class="px-2" for="city">{{ __('Città') }}</label>
-                <input type="text" name="city" id="city" value="{{ old('city', '') }}">
+                <input type="text" name="city" id="city" value="{{ old('city', '') }}" required
+                    maxlength="255">
                 @error('city')
                     <h5 class="text-danger ml-4">{{ $message }}</h5>
                 @enderror
@@ -139,7 +149,7 @@
             <div class="p-3">
                 <label class="px-2" for="countrySubdivision">{{ __('Provincia') }}</label>
                 <input type="text" name="countrySubdivision" id="countrySubdivision"
-                    value="{{ old('countrySubdivision', '') }}">
+                    value="{{ old('countrySubdivision', '') }}" required maxlength="255">
                 @error('countrySubdivision')
                     <h5 class="text-danger ml-4">{{ $message }}</h5>
                 @enderror
