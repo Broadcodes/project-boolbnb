49d75573fbacf8f51cff27e972d25487ec51ecaa
Anzelo7 <andrea.anzellotti7@gmail.com>
2022-12-14 17:27:08 +0100

    aa HomeGuest.blade > popup_Login

--- resources/views/layouts/homeGuests.blade.php
+++ resources/views/layouts/homeGuests.blade.php
@@ -12,7 +12,9 @@
     <link rel="shortcut icon" href="" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Nunito+Sans&family=Roboto&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans&family=Roboto&display=swap" rel="stylesheet">
 
     <script type="text/javascript" src="{{ asset('js/homeGuest.js') }}" defer></script>
     <script src="https://kit.fontawesome.com/2b5def4d88.js" crossorigin="anonymous"></script>
@@ -29,14 +31,95 @@
 
             <ul class="d-flex m-0">
                 <li class="nav-item">
-                    <a class="btn btn-lg" href="{{ route('login') }}">Login</a>
+                    <button class="btn btn-lg" data-toggle="modal" data-target="#loginModal"
+                        href="{{ route('login') }}">Login</button>
                 </li>
                 <li class="nav-item">
-                    <a class="btn btn-lg" href="{{ route('register') }}">Register</a>
+                    <button class="btn btn-lg" href="{{ route('register') }}">Register</button>
                 </li>
             </ul>
         </div>
     </nav>
+
+    <!-- MODAL -->
+
+    <div class="card modal fade" wire:ignore.self id="addStudentModal" tabindex="-1" data-backdrop="static"
+        data-keyboard="false" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+        <div class="card-header">{{ __('Login') }}</div>
+
+        <div class="card-body">
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+
+                <div class="form-group row">
+                    <label for="email"
+                        class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                    <div class="col-md-6">
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                            name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                    <div class="col-md-6">
+                        <input id="password" type="password"
+                            class="form-control @error('password') is-invalid @enderror" name="password" required
+                            autocomplete="current-password">
+
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <div class="col-md-6 offset-md-4">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" name="remember" id="remember"
+                                {{ old('remember') ? 'checked' : '' }}>
+
+                            <label class="form-check-label" for="remember">
+                                {{ __('Remember Me') }}
+                            </label>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group row mb-0">
+                    <div class="col-md-8 offset-md-4">
+                        <button type="submit" class="btn btn-primary">
+                            {{ __('Login') }}
+                        </button>
+
+                        @if (Route::has('password.request'))
+                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                {{ __('Forgot Your Password?') }}
+                            </a>
+                        @endif
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    @push('scripts')
+        <script>
+            window.addEventListener('close-modal', event => {
+                $('#addStudentModal').modal('hide');
+            });
+        </script>
+    @endpush
 
     <!-- TYPE NAVBAR -->
 
