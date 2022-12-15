04cf656c12c18b8958b8a28392ab61a91a170120
FrenzH <franciattom99@gmail.com>
2022-12-15 16:33:46 +0000

    commit

--- app/Http/Controllers/apiGuest/apiCoordinateController.php
+++ app/Http/Controllers/apiGuest/apiCoordinateController.php
@@ -36,20 +36,31 @@ class apiCoordinateController extends Controller
         //put a temporary number in as it wont pass the lng in the JSON
 
         $sql="SELECT
-        *
-        ,((ACOS(SIN(". $coordinate['lat'] ." * PI() / 180) * SIN(`latitude` * PI() / 180) + COS(". $coordinate['lat'] ." * PI() / 180) * COS(`latitude` * PI() / 180) * COS((". $coordinate['lng'] ." - `longitude`) * PI() / 180)) * 180 / PI()) * 60 * 1.1515) AS `distance`
-        FROM `apartments`
-        WHERE
+        `id`,`latitude`,`longitude`,
         (
-          `latitude` BETWEEN (". $coordinate['lat'] ." - 10) AND (". $coordinate['lat'] ." + 10)
-          AND `longitude` BETWEEN (". $coordinate['lng'] ."- 10) AND (". $coordinate['lng'] ." + 10)
-        )
-        ORDER BY `distance` ASC
-        limit 25";
+            6371 *
+            acos(
+                cos( radians( " . $coordinate['lat'] . " ) ) *
+                cos( radians( `latitude` ) ) *
+                cos(
+                    radians( `longitude` ) - radians( " . $coordinate['lng'] . " )
+                ) +
+                sin(radians(" . $coordinate['lat'] . ")) *
+                sin(radians(`latitude`))
+            )
+        ) `distance`
+    FROM
+        `apartments`
+    HAVING
+        `distance` < 100
+    ORDER BY
+        `distance`
+    LIMIT
+        25";
 
         $apartments=DB::select($sql);
 
-        $data=array($apartments,$coordinate);
+        $data=array($apartments);
 
 
 
