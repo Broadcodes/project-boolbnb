186644ce6bbb3f8ee401074313ddf05f28c7f423
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-12-16 16:36:02 +0100

    fix

--- resources/js/view/homeGuest.vue
+++ resources/js/view/homeGuest.vue
@@ -8,15 +8,15 @@
             <div class="apartment_container container-fluid mx-5 px-5">
                 <div v-if="filteredData">
                     <div class="apartmentShow" v-for="apartment in apartments" :key="apartment.id" @click="getApartmentShow(apartment)">
-                        <img v-if="apartment.apartment_images == null" class="img-fluid w-25" src="../../../public/images/immagine_non_disponibile.png" alt="Nessuna immagine">
-                        <img v-else class="img-fluid w-25" :src="getSrcImages(apartment.apartment_images)" :alt="apartment.apartment_title">
+                        <img v-if="apartment.apartment_images == null" class="img-fluid w-25" :src="getSrcImages('images', 'immagine_non_disponibile.png')" alt="Nessuna immagine">
+                        <img v-else class="img-fluid w-25" :src="getSrcImages('storage', apartment.apartment_images)" :alt="apartment.apartment_title">
                         <h4> {{ apartment.apartment_title }}</h4>
                     </div>
                 </div>
                 <div v-else>
                     <div class="apartmentShow" v-for="apartment in apartmentToShow" :key="apartment.id" @click="getApartmentShow(apartment)">
-                        <img v-if="apartment.apartment_images == null" class="img-fluid w-25" src="../../../public/images/immagine_non_disponibile.png" alt="Nessuna immagine">
-                        <img v-else class="img-fluid w-25" :src="getSrcImages(apartment.apartment_images)" :alt="apartment.apartment_title">
+                        <img v-if="apartment.apartment_images == null" class="img-fluid w-25" :src="getSrcImages('images', 'immagine_non_disponibile.png')" alt="Nessuna immagine">
+                        <img v-else class="img-fluid w-25" :src="getSrcImages('storage', apartment.apartment_images)" :alt="apartment.apartment_title">
                         <h4> {{ apartment.apartment_title }}</h4>
                     </div>
 
@@ -88,8 +88,8 @@ export default {
             })
             this.filteredData = false;
         },
-        getSrcImages(path){
-            return 'storage/' + path;
+        getSrcImages(folder, path){
+            return folder + '/' + path;
         }
     },
 
