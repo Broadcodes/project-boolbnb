d7dfef54e2a97d172e0fb944e1880f698e006c92
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-12-22 23:46:31 +0100

    remove buttons "statistiche e sponsor"

--- resources/views/ura/dashboard.blade.php
+++ resources/views/ura/dashboard.blade.php
@@ -11,9 +11,10 @@
         <div class="container">
             <div class="row">
                 <div class="col">
-                    <div class="nc-accountInfo d-flex">
+                    <div class="nc-accountInfo d-flex align-items-center">
                         <div class="nc-card d-flex flex-column align-items-center">
-                            <img class="img-fluid nc-profilePic mt-3" src="{{ url('/images/profile-pic.png') }}" alt="profile pic">
+                            <img class="img-fluid nc-profilePic mt-3" src="{{ url('/images/profile-pic.png') }}"
+                                alt="profile pic">
                             <h3>{{ $user->name }} {{ $user->surname }}</h3>
                             <div class="personalInfo">
                                 <p>
@@ -26,20 +27,20 @@
                                 </p>
                             </div>
                         </div>
-                        <div class="nc-userAction d-flex flex-column justify-content-around">
+                        <div class="nc-userAction d-flex justify-content-start">
                             <div>
                                 <p>
                                     Ciao, <strong>{{ $user->name }}</strong>
                                 <p>
                                 <h5>Su BoolBnb dal: {{ $onBoolBnb }}</h5>
                             </div>
-                            <div>
-                                <hr>
+                            <div class="links">
+
                                 <a class="btn btn-primary my-4" href="{{ route('ura.apartments.index') }}">Miei
                                     Appartamenti</a>
-                                <a class="btn btn-success mx-2">Statistiche Appartamenti</a>
-                                <a class="btn btn-danger">Sponsorizza un appartamento</a>
-                                <hr>
+                                {{-- <a class="btn btn-success mx-2">Statistiche Appartamenti</a>
+                                <a class="btn btn-danger">Sponsorizza un appartamento</a> --}}
+
                             </div>
 
                         </div>
